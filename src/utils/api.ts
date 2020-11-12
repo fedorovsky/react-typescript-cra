7dfe335d4f5816f87c4a6b51e3f1731867e38242
a.fedorovskyi <a.fedorovskyi@plarium.com>
2020-11-12 20:40:11 +0200

    http

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,11 +1,5 @@
 const http = <T>(request: RequestInfo): Promise<T> => {
-  return new Promise((resolve) => {
-    fetch(request)
-      .then((response) => response.json())
-      .then((body) => {
-        resolve(body);
-      });
-  });
+  return fetch(request).then((response) => response.json());
 };
 
 export default http;
