05ebca0e169e11df41598559f3a6f54bf4f113ed
fedorovsky <anton.fedorovsky@gmail.com>
2021-03-12 06:41:20 +0000

    Deploying to gh-pages from  @ 13d2d3e1292f052effec3103282049b4a4d98411 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://fedorovsky.github.io/code-react-typescript/precache-manifest.6ff70e9d62fb2febbcb7b1ba4b3847b2.js"
+  "https://fedorovsky.github.io/code-react-typescript/precache-manifest.bd06c3d8c8b15e58835a00d56c2681e5.js"
 );
 
 self.addEventListener('message', (event) => {
