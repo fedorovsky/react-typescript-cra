d18fd3a18783c6b2fa5cddd587edf97dfe246530
fedorovsky <anton.fedorovsky@gmail.com>
2022-11-05 20:32:22 +0000

    Deploying to gh-pages from @ fedorovsky/react-typescript-cra@42bec1f901f845b393f14b832c19077c7a14bdaa 🚀

---
+++ precache-manifest.c8e4009970b656688470b816aec7a7c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4c780ffa338e9d6a5bbbff3e69d5130",
+    "url": "https://fedorovsky.github.io/react-typescript-cra/index.html"
+  },
+  {
+    "revision": "7f393359a362f9ed4b3d",
+    "url": "https://fedorovsky.github.io/react-typescript-cra/static/js/2.a928a6d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "https://fedorovsky.github.io/react-typescript-cra/static/js/2.a928a6d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dceb4d6f5fa2fd757839",
+    "url": "https://fedorovsky.github.io/react-typescript-cra/static/js/main.08993b9e.chunk.js"
+  },
+  {
+    "revision": "6ace10d06111e23febac",
+    "url": "https://fedorovsky.github.io/react-typescript-cra/static/js/runtime-main.ad9f02a5.js"
+  }
+]);
