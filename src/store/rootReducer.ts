15d03b5914b267070da6f56899d99673113992ea
a.fedorovskyi <a.fedorovskyi@palrium.com>
2023-01-08 18:39:38 +0100

    react 18

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,13 +1,9 @@
 import { combineReducers } from 'redux';
-import { connectRouter } from 'connected-react-router';
-import { History } from 'history';
-// import { usersReducer } from './users/users';
 import usersReducer from './slices/usersSlice';
 import { integrationReducer } from './integration';
 
-const rootReducer = (history: History) =>
+const rootReducer = () =>
   combineReducers({
-    router: connectRouter(history),
     users: usersReducer,
     integration: integrationReducer,
   });
