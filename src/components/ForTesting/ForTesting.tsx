01946e34dec03e1148b13520751980c1dba20fd6
a.fedorovskyi <a.fedorovskyi@palrium.com>
2023-01-10 21:22:13 +0100

    added redux-logger

---
+++ src/components/ForTesting/ForTesting.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const ForTesting: React.FC = () => {
+  return <div>Hello Component</div>;
+};
+
+export default ForTesting;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { createLogger } from 'redux-logger';
 import rootReducer from './rootReducer';
 
-// browser history
+const logger = createLogger({
+  collapsed: true,
+});
 
 const store = configureStore({
   reducer: rootReducer(),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
